8ddc2e23086d25ad73ec7278c9f9675fb9f0ea21
Megan <megan0597@hotmail.com>
2016-05-03 14:54:49 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,24 +1,15 @@
 <?php
 
-////LOCAL HOST
-//// DB settings
-//$user = 'root';
-//$password = '';
-//$db = 'herkansingimp03';
-//
-//$dbLink = mysqli_connect('localhost', $user, $password, $db); //connection to database
-
-//STUDENTEN SPACE
-//settings
-$host = 'localhost';
-$user = '0892322';
-$password = '1f18d59e';
-$db = '0892322';
+// db settings
+$host = 'fill in your own here';
+$user = 'fill in your own here';
+$password = 'fill in your own here';
+$db = 'fill in your own here';
 
 
 $dbLink = mysqli_connect($host, $user, $password, $db); //connection to database
 
 // API settings
-define("YOUTUBE_API_KEY", "AIzaSyDH_BGx70ypPGdnnD7Rvnitc1LrmgB1Euk");
+define("YOUTUBE_API_KEY", "Fill in your own key here");
 
 
